a43f8ddcfe1a54f73f62cc08c49e770839f04b7c
iLouis1 <rhiganakatugba@gmail.com>
2023-06-27 15:51:01 -0700

    Get size

--- 5-get_size.c
+++ 5-get_size.c
@@ -16,15 +16,12 @@ int get_size(const char *format, int *i)
 
 	if (format[curr_i] == '1')
 		size = S_LONG;
-	
 	else if (format[curr_i] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
 		*i = curr_i - 1;
-	
 	else
 		*i = curr_i;
-	
 	return (size);
 }
